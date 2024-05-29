3bd66d431fc2597f32599b60a74133b5f1cc487f
Léo Riera <leo.riera1@gmail.com>
2024-05-30 01:47:44 +0200

    hypridle and hyprlock

--- install_arch.sh
+++ install_arch.sh
@@ -3,6 +3,6 @@
 # Install packages fonts \ hyprland \ tools \ app
 sudo pacman -S \
 noto-fonts-emoji ttf-font-awesome ttf-jetbrains-mono-nerd \
-alacritty dunst hyprpaper hyprland qt5-wayland qt6-wayland waybar wayland wofi \
+alacritty dunst hyprland hypridle hyprlock hyprpaper qt5-wayland qt6-wayland waybar wayland wofi \
 dolphin nwg-look openssh pipewire grim slurp \
 discord firefox
