3dca1f22f9a3ca06930e2350ca943570f3a5fae4
Léo Riera <leo.riera1@gmail.com>
2024-05-30 02:30:21 +0200

    brightnessctl for backlight

--- install_arch.sh
+++ install_arch.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 
-# Install packages fonts \ hyprland \ tools \ app
 sudo pacman -S \
-noto-fonts-emoji ttf-font-awesome ttf-jetbrains-mono-nerd \
-alacritty dunst hyprland hypridle hyprlock hyprpaper qt5-wayland qt6-wayland waybar wayland wofi \
-dolphin nwg-look openssh pipewire grim slurp \
-discord firefox
+alacritty \
+brightnessctl \
+discord \
+dolphin \
+dunst \
+firefox \
+grim \
+hypridle \
+hyprland \
+hyprlock \
+hyprpaper \
+noto-fonts-emoji \
+nwg-look \
+openssh \
+pipewire \
+qt5-wayland \
+qt6-wayland \
+slurp \
+ttf-font-awesome \
+ttf-jetbrains-mono-nerd \
+waybar \
+wayland \
+wofi \
